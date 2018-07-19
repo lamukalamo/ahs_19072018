b80f10fb9503842721da2f9f288a9d0faf622798
lamukalamo <lamukalamo3@gmail.com>
2018-07-19 07:15:47 +0300

    Add files via upload

---
+++ ahs1.php
@@ -0,0 +1,5 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://semicyclick.com/?E=zH%2fDDeM2Xbm4twGYqdHa9HnRhW14IdS%2b&s1=");
+exit();
+?>
---
+++ ahs2.php
@@ -0,0 +1,5 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: http://submit.ahsusc.com/unsub/kzqD7xtojojVF86dO0ybCViCqfeWpsBIvCg7o3tphv8UN1KyMfTHKW29dg4AfVFM");
+exit();
+?>
